deed024ce412814c4d11e6c921bc6460f58bf458
147258369147258369 <52413500+147258369147258369@users.noreply.github.com>
2022-09-17 16:06:39 +0300

    Add files via upload

---
+++ lab1.3.py
@@ -0,0 +1,15 @@
+fib1 = 0
+fib2 = 1
+fib_sum = 1
+n = int(input())
+if n == fib1 or n == fib2:
+    print('Yes')
+elif n != fib_sum:
+    while fib_sum < n:
+        fib1 = fib2
+        fib2 = fib_sum
+        fib_sum = fib1 + fib2
+    if n == fib_sum:
+        print("Yes")
+    else:
+        print("No")
